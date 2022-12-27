20572b18422740127e2568f125e095c092261e9f
MARCO <marco@Marcos-Mac-mini.homenet.telecomitalia.it>
2022-12-27 22:16:36 +0100

    commit

--- bzip2/bwt_test.go
+++ bzip2/bwt_test.go
@@ -1,117 +1,59 @@
 package bzip2
 
 import (
+	"bytes"
+	"math/rand"
 	"testing"
+	"time"
 
-	"github.com/marco-spagnuolo/unisacompression/internal/testutil"
+	"github.com/marco-spagnuolo/unisacompression/bzip2/bwt"
 )
 
-func TestBurrowsWheelerTransform(t *testing.T) {
-	vectors := []struct {
-		input  []byte // The input test string
-		output []byte // Expected output string after BWT
-		ptr    int    // The BWT origin pointer
-	}{{
-		input:  []byte(""),
-		output: []byte(""),
-		ptr:    -1,
-	}, {
-		input:  []byte("Hello, world!"),
-		output: []byte(",do!lHrellwo "),
-		ptr:    3,
-	}, {
-		input:  []byte("SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES"),
-		output: []byte("TEXYDST.E.IXIXIXXSSMPPS.B..E.S.EUSFXDIIOIIIT"),
-		ptr:    29,
-	}, {
-		input:  []byte("0123456789"),
-		output: []byte("9012345678"),
-		ptr:    0,
-	}, {
-		input:  []byte("9876543210"),
-		output: []byte("1234567890"),
-		ptr:    9,
-	}, {
-		input:  []byte("The quick brown fox jumped over the lazy dog."),
-		output: []byte("kynxederg.l ie hhpv otTu c uwd rfm eb qjoooza"),
-		ptr:    9,
-	}, {
-		input: []byte("" +
-			"Mary had a little lamb, its fleece was white as snow" +
-			"Mary had a little lamb, its fleece was white as snow" +
-			"Mary had a little lamb, its fleece was white as snow" +
-			"Mary had a little lamb, its fleece was white as snow" +
-			"Mary had a little lamb, its fleece was white as snow" +
-			"Mary had a little lamb, its fleece was white as snow" +
-			"Mary had a little lamb, its fleece was white as snow" +
-			"Mary had a little lamb, its fleece was white as snow" +
-			"Nary had a little lamb, its fleece was white as snow"),
-		output: []byte("" +
-			"dddddddddeeeeeeeeesssssssssyyyyyyyyy,,,,,,,,,eeeeeee" +
-			"eeaaaaaaaaassssssssseeeeeeeeesssssssssbbbbbbbbbwwwww" +
-			"wwww         hhhhhhhhhlllllllllNMMMMMMMM         www" +
-			"wwwwwwmmmmmmmmmeeeeeeeeeaaaaaaaaatttttttttlllllllllc" +
-			"cccccccceeeeeeeeelllllllll                  wwwwwwww" +
-			"whhhhhhhhh         lllllllll         tttttttttffffff" +
-			"fff         aaaaaaaaasssssssssnnnnnnnnnaaaaaaaaatttt" +
-			"tttttaaaaaaaaaaaaaaaaaa         iiiiiiiiitttttttttii" +
-			"iiiiiiiiiiiiiiiiooooooooo                  rrrrrrrrr"),
-		ptr: 99,
-	}, {
-		input: []byte("" +
-			"AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTCTCTGAC" +
-			"AGCAGCTTCTGAACTGGTTACCTGCCGTGAGTAAATTAAAATTTTATTGACTTAGGTCACTAAA" +
-			"TACTTTAACCAATATAGGCATAGCGCACAGACAGATAAAAATTACAGAGTACACAACATCCATG" +
-			"AAACGCATTAGCACCACCATTACCACCACCATCACCACCACCATCACCATTACCATTACCACAG" +
-			"GTAACGGTGCGGGCTGACGCGTACAGGAAACACAGAAAAAAGCCCGCACCTGACAGTGCGGGCT" +
-			"TTTTTTTCGACCAAAGGTAACGAGGTAACAACCATGCGAGTGTTGAAGTTCGGCGGTACATCAG" +
-			"TGGCAAATGCAGAACGTTTTCTGCGGGTTGCCGATATTCTGGAAAGCAATGCCAGGCAGGGGCA"),
-		output: []byte("" +
-			"TAGAATAAATGGAGACTCTAATACTCTACTGGAAACAGACCACAAACATACCTGGTCGTAGATT" +
-			"CCCCCCATCCCTAAGAAACGAGTCCCCACATCATCACCTCGACTGGGCCGAGACTAAGCCCCCA" +
-			"ACTGAACCCCCTTACGAAGGCGGAAGCTCCGCCCTGTAGAAAAGACGAATGCCAACCCCCGTAA" +
-			"AAAAAAGAATAAAAGGCGAATAGCGCAATAGGGGAGCAATTTTCGTACTTATAGAGGAGTGATT" +
-			"ATTCTTTCTAACACGGTGGACACTAGGCTATTTATTTGCGAAGATTTGGAACGGGCCCACAAAC" +
-			"ACTGAGGGACGGATCGATATAGATGCTATCGGTGGGTGGTTTTATAATAAATAAGATATTGGTC" +
-			"TTTCACTCCCCTGCAATCAGGCCGGCAGCGAATAAAAGACTTTGCATAGAGCTTTTACTGTTTC"),
-		ptr: 99,
-	}, {
-		input:  testutil.MustLoadFile("testdata/gauntlet_test3.bin"),
-		output: testutil.MustLoadFile("testdata/gauntlet_test3.bwt"),
-		ptr:    0,
-	}, {
-		input:  testutil.MustLoadFile("testdata/silesia_ooffice.bin"),
-		output: testutil.MustLoadFile("testdata/silesia_ooffice.bwt"),
-		ptr:    461,
-	}, {
-		input:  testutil.MustLoadFile("testdata/silesia_xray.bin"),
-		output: testutil.MustLoadFile("testdata/silesia_xray.bwt"),
-		ptr:    1532,
-	}, {
-		input:  testutil.MustLoadFile("testdata/testfiles_test3.bin"),
-		output: testutil.MustLoadFile("testdata/testfiles_test3.bwt"),
-		ptr:    0,
-	}, {
-		input:  testutil.MustLoadFile("testdata/testfiles_test4.bin"),
-		output: testutil.MustLoadFile("testdata/testfiles_test4.bwt"),
-		ptr:    1026,
-	}}
+func TestEncode1(t *testing.T) {
+	// Generate random input of length 12
+	input := []byte(generateRandomString(12))
 
-	bwt := new(burrowsWheelerTransform)
-	for i, v := range vectors {
-		output := append([]byte(nil), v.input...)
-		ptr := bwt.Encode(output)
-		input := append([]byte(nil), v.output...)
-		bwt.Decode(input, ptr)
-
-		if got, want, ok := testutil.BytesCompare(input, v.input); !ok {
-			t.Errorf("test %d, input mismatch:\ngot  %s\nwant %s", i, got, want)
-		}
-		if got, want, ok := testutil.BytesCompare(output, v.output); !ok {
-			t.Errorf("test %d, output mismatch:\ngot  %s\nwant %s", i, got, want)
-		}
-		if ptr != v.ptr {
-			t.Errorf("test %d, pointer mismatch: got %d, want %d", i, ptr, v.ptr)
-		}
+	// Call the Encode1 function
+	output, err := bwt.Encode1(input)
+	if err != nil {
+		t.Error(err)
 	}
+
+	// Check that the output is not empty
+	if len(output) == 0 {
+		t.Error("Expected non-empty output")
+	}
+}
+
+func TestDecode1(t *testing.T) {
+	// Generate random input of length 12
+	input := []byte(generateRandomString(12))
+
+	// Encode the input using Encode1
+	encoded, err := bwt.Encode1(input)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Call the Decode1 function
+	output, err := bwt.Decode1(encoded)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Check that the output is as expected
+	if !bytes.Equal(output, input) {
+		t.Errorf("Expected output %v but got %v", input, output)
+	}
+}
+
+// Helper function to generate a random string of a given length
+func generateRandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	chars := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	result := make([]rune, length)
+	for i := range result {
+		result[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(result)
 }
