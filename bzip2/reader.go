44034084ed47681c5d7e45bdeb0d9fdc1eb7872c
theveloper <81595718+marco-spagnuolo@users.noreply.github.com>
2022-12-27 17:02:08 +0100

    Update reader.go

--- bzip2/reader.go
+++ bzip2/reader.go
@@ -195,7 +195,7 @@ func (zr *Reader) decodeBlock() []byte {
 	if ptr >= len(buf) {
 		panicf(errors.Corrupted, "origin pointer (0x%06x) exceeds block size: %d", ptr, len(buf))
 	}
-	zr.bwt.Decode1(buf, ptr)
+	zr.bwt.Decode(buf, ptr)
 
 	return buf
 }
